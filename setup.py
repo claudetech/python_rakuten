009238b62d973bd3649db91540de85774cb16ac2
Daniel Perez <tuvistavie@gmail.com>
2014-08-31 19:05:50 +0900

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rakuten',
-    version='0.1.2',
+    version='0.1.3',
     author='Daniel Perez',
     author_email='daniel@claudetech.com',
     packages=['rakuten', 'rakuten.apis'],
